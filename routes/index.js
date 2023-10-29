6d4ed3700f4fe09af78936907363bc73d3a7ee2a
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-10-28 23:29:21 -0600

    updated swagger files and dependencies

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/exercises', require('./exercises'));
-router.use('/users', require('./users'));
 
 module.exports = router;
