32f256f304459cf4f0f2ccf8e0501546933256d4
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-11-04 23:30:49 -0600

    added oauth

--- server.js
+++ server.js
@@ -1,33 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 const app = express();
-const {auth, requiresAuth} = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
-//oauth
 require('dotenv').config();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.GITHUB_CLIENT_SECRET,
-  baseURL: process.env.GITHUB_BASE_URL,
+app  
+  .use(session({
+    secret: process.env.GITHUB_CLIENT_SECRET,
+    resave: true,
+    saveUninitialized: true
+  }))
+  .use(passport.initialize())
+  .use(passport.session());
+
+passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
-  issuerBaseURL: process.env.GITHUB_ISSUER_BASE_URL,
-};
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.GITHUB_CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
-  .use('/', require('./routes'))
-  .use(auth(config))
-  .get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
   });
+
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send('Logged in');
+    app.use('/', require('./routes'));
+  } else {
+    res.send('Logged out');
+  }
+});
 
 mongodb.initDb((err) => {
   if (err) {
